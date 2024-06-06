bdf5d03250a92e308d4847b21e2de3e4a5fe6f4f
Callum Green <155394625+callum-green24@users.noreply.github.com>
2024-06-07 10:55:14 +1200

    add db functions

---
+++ server/db/db.ts
@@ -0,0 +1,91 @@
+import db from './connection.ts'
+import { Drink } from '../../models/cocktail.ts'
+
+export async function getAllDrinks() {
+  return await db('favourites').select()
+}
+
+export async function addDrink(data: Drink) {
+  const {
+    idDrink,
+    strDrink,
+    strAlcoholic,
+    strGlass,
+    strInstructions,
+    strDrinkThumb,
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+    strIngredient11,
+    strIngredient12,
+    strIngredient13,
+    strIngredient14,
+    strIngredient15,
+    strMeasure1,
+    strMeasure2,
+    strMeasure3,
+    strMeasure4,
+    strMeasure5,
+    strMeasure6,
+    strMeasure7,
+    strMeasure8,
+    strMeasure9,
+    strMeasure10,
+    strMeasure11,
+    strMeasure12,
+    strMeasure13,
+    strMeasure14,
+    strMeasure15,
+    strImageSource,
+  } = data
+  return await db('favourties').insert({
+    idDrink,
+    strDrink,
+    strAlcoholic,
+    strGlass,
+    strInstructions,
+    strDrinkThumb,
+    strIngredient1,
+    strIngredient2,
+    strIngredient3,
+    strIngredient4,
+    strIngredient5,
+    strIngredient6,
+    strIngredient7,
+    strIngredient8,
+    strIngredient9,
+    strIngredient10,
+    strIngredient11,
+    strIngredient12,
+    strIngredient13,
+    strIngredient14,
+    strIngredient15,
+    strMeasure1,
+    strMeasure2,
+    strMeasure3,
+    strMeasure4,
+    strMeasure5,
+    strMeasure6,
+    strMeasure7,
+    strMeasure8,
+    strMeasure9,
+    strMeasure10,
+    strMeasure11,
+    strMeasure12,
+    strMeasure13,
+    strMeasure14,
+    strMeasure15,
+    strImageSource,
+  })
+}
+
+export async function deleteDrink(id: string) {
+  await db('favourites').where(id).del()
+}
